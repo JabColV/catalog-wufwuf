7504b1c65d57e52b5c85a25ceeff7e9e2b951f1b
Juan Pablo <carabali.juan@correounivalle.edu.co>
2024-04-19 19:15:40 -0500

    Added method for selecting a pet by id

--- backend/mascotas/views.py
+++ backend/mascotas/views.py
@@ -21,3 +21,12 @@ def create_pet(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_pet(request, pk):
+    try:
+        mascota = Mascota.objects.get(pk=pk)
+    except Mascota.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = MascotaSerializer(mascota)
+    return Response(serializer.data)
