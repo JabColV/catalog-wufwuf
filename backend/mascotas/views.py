e724dbeb8e1743e7b58e71c4bb8ca14e4db7e423
Nicol <Nicol Ortiz>
2024-05-25 14:54:18 -0500

    changes

--- backend/mascotas/views.py
+++ backend/mascotas/views.py
@@ -1,11 +1,10 @@
 from .serializers import MascotaSerializer
 from .models import Mascota
 from rest_framework.response import Response
-from rest_framework.decorators import api_view 
+from rest_framework.decorators import api_view
 from rest_framework import status
-from django.core.files.storage import FileSystemStorage
 from django.http import JsonResponse
-
+import json
 
 @api_view(['GET'])
 def mascota_list(request):
@@ -15,43 +14,16 @@ def mascota_list(request):
 
 @api_view(['POST'])
 def create_pet(request):
-    if request.method == 'POST': 
-        # Lista para almacenar los nombres de los archivos guardados
-        uploaded_file_names = []
-        # Lista para almacenar las URLs de los archivos guardados
-        urls_images = [] 
-        # Verificar si hay archivos adjuntos
-        if request.FILES:
-            # Iterar sobre los archivos adjuntos
-            for key, uploaded_file in request.FILES.items():
-                # Ubicación donde se guardará el archivo
-                fs = FileSystemStorage(location='./mascotas/static/')
-                # nombre del archivo
-                filename = fs.save(uploaded_file.name, uploaded_file)
-                # Añadir el nombre del archivo a la lista de nombres de archivos guardados
-                uploaded_file_names.append(filename)
-                # Ruta del archivo guardado
-                saved_file_path = fs.url(filename)
-        # Verificar si se enviaron datos simples en el request        
-        if request.POST:
-            # Convertir los datos simples a un diccionario
-            modified_data = request.POST.dict()
-            # iterar sobre los nombres de los archivos guardados
-            for file in uploaded_file_names:
-                # Construir la URL para el archivo guardado
-                file_url = f'http://127.0.0.1:8000/static/{file}'
-                # Añadir la URL a la lista de URLs de archivos guardados
-                urls_images.append(file_url)
-            # Añadir la lista de URLs de archivos guardados al diccionario de datos
-            modified_data['urls_images'] = urls_images
-            # Crear una instancia del serializador con los datos modificados
-            serializer = MascotaSerializer(data=modified_data)
-            # Verificar si los datos son válidos
-            if serializer.is_valid():
-                # Guardar los datos en la base de datos
-                serializer.save()
-                return JsonResponse({'message': 'Datos y Archivos guardados exitosamente'}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # Convertir el cuerpo de la solicitud en un objeto Python
+    request_data = json.loads(request.body)   
+    # Crear una instancia del serializador con los datos modificados
+    serializer = MascotaSerializer(data=request_data)
+    # Verificar si los datos son válidos
+    if serializer.is_valid():
+        # Guardar los datos en la base de datos
+        serializer.save()
+        return JsonResponse({'message': 'Datos y Archivos guardados exitosamente'}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
