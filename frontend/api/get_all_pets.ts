da3aedb4e93a8f4ca935586464a0f657f3e60bff
Nicol <Nicol Ortiz>
2024-05-25 15:58:53 -0500

    going back a little bit

--- frontend/api/get_all_pets.ts
+++ frontend/api/get_all_pets.ts
@@ -1,3 +1,50 @@
+// /**
+//  * @file fetch_pets.ts
+//  * @version 1.0.0
+//  * @description Este archivo se encarga de realizar la petición para obtener a todas las mascotas.
+//  * @author Nicol Ortiz
+//  * @contact nicol.ortiz@correounivalle.edu.co
+//  * @date 19 de abril del 2024
+//  */
+
+// import { QueryFunction } from "react-query";
+// import { PetsAPIResponse } from "@types/types";
+
+// /**
+//  * @async
+//  * @function fetchPets
+//  * @param {void}
+//  * @returns {Promise<PetsAPIResponse>}
+//  * @description Esta función se encarga de realizar la petición para obtener a todas las mascotas.
+//  */
+
+// const fetchPets: QueryFunction<PetsAPIResponse, ["list_pets"]> = async () => {
+//   try {
+//     const apiRes = await fetch(
+//       `${process.env.NEXT_PUBLIC_FRONTEND_URL}/api`
+//     );
+//     // const apiRes = await fetch(
+//     //   `${process.env.NEXT_PUBLIC_FRONTEND_URL_KUBERNETES}/api`
+//     // );
+
+//     if (!apiRes.ok) {
+//       throw new Error(`Error fetching /mascotas: ${apiRes.statusText}`);
+//     }
+
+//     const data = await apiRes.json();
+
+//     return data.data
+
+//   } catch (error) {
+//     console.error("Error en fetchPets:", error);
+//     throw error;
+//   }
+// };
+
+// export default fetchPets;
+
+
+
 /**
  * @file fetch_pets.ts
  * @version 1.0.0
@@ -18,23 +65,17 @@ import { PetsAPIResponse } from "@types/types";
  * @description Esta función se encarga de realizar la petición para obtener a todas las mascotas.
  */
 
-const fetchPets: QueryFunction<PetsAPIResponse, ["list_pets"]> = async () => {
+const fetchPets: QueryFunction<PetsAPIResponse, ["pets"]> = async () => {
   try {
     const apiRes = await fetch(
-      `${process.env.NEXT_PUBLIC_FRONTEND_URL}/api`
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/mascotas/`
     );
-    // const apiRes = await fetch(
-    //   `${process.env.NEXT_PUBLIC_FRONTEND_URL_KUBERNETES}/api`
-    // );
 
     if (!apiRes.ok) {
       throw new Error(`Error fetching /mascotas: ${apiRes.statusText}`);
     }
 
-    const data = await apiRes.json();
-
-    return data.data
-
+    return apiRes.json();
   } catch (error) {
     console.error("Error en fetchPets:", error);
     throw error;
